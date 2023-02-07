8fed48e6ab9290791fe16e949aa9b562ed72ca63
Aaiden Witten <aaidenplays@yahoo.com>
2023-02-06 19:22:38 -0600

    fin

--- lib/dog.py
+++ lib/dog.py
@@ -1,27 +1,44 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import ipdb
+from models import Dog
 
 engine = create_engine('sqlite:///:memory:')
+# Session = sessionmaker(bind=engine)
+# session = Session()
 
 def create_table(base):
-    pass
+    base.metadata.create_all(engine)
+    return engine
+    
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+    return session
 
-def new_from_db(session):
-    pass
+def new_from_db(session, dog_from_db): #somn aint right here
+    # ipdb.set_trace()
+    return dog_from_db
 
 def get_all(session):
-    pass
+    query = session.query(Dog)
+    return query
+    
 
 def find_by_name(session, name):
-    pass
+    query = session.query(Dog).filter(Dog.name == name).first()
+    return query
+    
 
 def find_by_id(session, id):
-    pass
+    query = session.query(Dog).filter(Dog.id == id).first()
+    return query
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    query = session.query(Dog).filter(Dog.name == name and Dog.breed == breed).first()
+    return  query
 
 def update_breed(session, dog, breed):
-    pass
+    query = session.query(Dog).filter(Dog.name == dog.name and Dog.breed == dog.breed).update({Dog.breed: breed})
+    return query
